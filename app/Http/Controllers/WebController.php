d363313bbc3fdb0dd52b6ae23a47f86d9ed51282
Marek Mikula <marek@justitpro.com>
2025-05-04 19:40:21 +0200

    feat: removed code

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,18 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
-use Support\Classifier\Enums\ClassifierTypeEnum;
-use Support\Classifier\Repositories\ClassifierRepositoryInterface;
 
 class WebController extends Controller
 {
     public function welcome(): View
     {
-        /** @var ClassifierRepositoryInterface $repo */
-        $repo = app(ClassifierRepositoryInterface::class);
-
-        dd($repo->getValuesForType(ClassifierTypeEnum::PHONE_PREFIX)->pluck('label')->toArray());
-
         return view('welcome');
     }
 }
