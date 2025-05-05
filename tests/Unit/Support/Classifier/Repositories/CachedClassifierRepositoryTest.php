a939d6425c1b5ac847fe1c8b1550f9308578c58b
Marek Mikula <marek@justitpro.com>
2025-05-05 11:27:10 +0200

    feat: added test for cachced repository

---
+++ tests/Unit/Support/Classifier/Repositories/CachedClassifierRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Support\Classifier\Repositories;
+
+use Support\Classifier\Repositories\ClassifierRepositoryInterface;
+
+beforeEach(function (): void {
+    // turn on caching of classifiers, so we
+    // know that we use cached repository
+    config()->set('classifier.cache_enabled', true);
+});
+
+/** @covers \Support\Classifier\Repositories\ClassifierRepository::getValuesForType */
+it('tests getValuesForType method', function (): void {
+    /** @var ClassifierRepositoryInterface $repository */
+    $repository = app(ClassifierRepositoryInterface::class);
+
+    // todo
+})->todo();
+
+/** @covers \Support\Classifier\Repositories\ClassifierRepository::getValuesForTypes */
+it('tests getValuesForTypes method', function (): void {
+    /** @var ClassifierRepositoryInterface $repository */
+    $repository = app(ClassifierRepositoryInterface::class);
+
+    // todo
+})->todo();
