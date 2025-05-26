768480d534c1770281c0f0e52c9f5c890b46ecb8
Marek Mikula <marek@justitpro.com>
2025-05-26 18:09:58 +0200

    feat: classifier endpoint

--- src/Support/Classifier/Http/Controllers/ClassifierController.php
+++ src/Support/Classifier/Http/Controllers/ClassifierController.php
@@ -6,15 +6,29 @@ namespace Support\Classifier\Http\Controllers;
 
 use App\Enums\ResponseCodeEnum;
 use App\Http\Controllers\ApiController;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Support\Classifier\Enums\ClassifierTypeEnum;
+use Support\Classifier\Http\Requests\ClassifierIndexRequest;
 use Support\Classifier\Http\Requests\ClassifierListRequest;
 use Support\Classifier\Http\Resources\Collections\ClassifierCollection;
+use Support\Classifier\UseCases\GetClassifierIndexUseCase;
 use Support\Classifier\UseCases\GetClassifierListUseCase;
 
 class ClassifierController extends ApiController
 {
-    public function getList(ClassifierListRequest $request, ClassifierTypeEnum $type): JsonResponse
+    public function index(ClassifierIndexRequest $request): JsonResponse
+    {
+        $index = GetClassifierIndexUseCase::make()->handle($request->getTypes());
+
+        $index = array_map(fn (Collection $classifiers) => new ClassifierCollection($classifiers), $index);
+
+        return $this->jsonResponse(ResponseCodeEnum::SUCCESS, [
+            'classifiers' => $index,
+        ]);
+    }
+
+    public function list(ClassifierListRequest $request, ClassifierTypeEnum $type): JsonResponse
     {
         $list = GetClassifierListUseCase::make()->handle($type);
 
