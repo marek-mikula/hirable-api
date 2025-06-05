77b3c703596be21a40b402b54fc03533e2111cf2
Marek Mikula <marek@justitpro.com>
2025-06-05 11:31:19 +0200

    feat: builder tests

---
+++ tests/Unit/Domain/Position/Models/PositionBuilderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Position\Models;
+
+use Domain\Position\Enums\PositionStateEnum;
+use Domain\Position\Models\Position;
+
+use function Tests\Common\Helpers\assertCollectionsAreSame;
+
+/** @covers \Domain\Position\Models\Builders\PositionBuilder::approvalExpired */
+it('tests approvalExpired method', function (): void {
+    Position::factory()->ofApproveUntil(now())->ofState(PositionStateEnum::APPROVAL_PENDING)->create();
+    Position::factory()->ofApproveUntil(now()->subDay())->ofState(PositionStateEnum::APPROVAL_APPROVED)->create();
+    $position = Position::factory()->ofApproveUntil(now()->subDay())->ofState(PositionStateEnum::APPROVAL_PENDING)->create();
+
+    $results = Position::query()->approvalExpired()->get();
+
+    assertCollectionsAreSame(collect([$position->id]), $results);
+});
