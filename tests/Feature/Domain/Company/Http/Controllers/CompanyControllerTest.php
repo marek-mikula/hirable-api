1706df72cf112563c19b8c41d22a40eaddfb36e5
Marek Mikula <marek@justitpro.com>
2025-06-05 10:19:39 +0200

    feat: add missing tests

--- tests/Feature/Domain/Company/Http/Controllers/CompanyControllerTest.php
+++ tests/Feature/Domain/Company/Http/Controllers/CompanyControllerTest.php
@@ -4,5 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Domain\Company\Http\Controllers;
 
+/** @covers \Domain\Company\Http\Controllers\CompanyController::show */
+it('CompanyController@show', function (): void {})->todo();
+
 /** @covers \Domain\Company\Http\Controllers\CompanyController::update */
 it('CompanyController@update', function (): void {})->todo();
