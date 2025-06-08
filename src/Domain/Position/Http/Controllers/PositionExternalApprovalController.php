f3426b8eb4bde433ac5ec8ad8acdc7f943da871d
Marek Mikula <marek@justitpro.com>
2025-06-08 13:14:56 +0200

    feat: removed useless resource

--- src/Domain/Position/Http/Controllers/PositionExternalApprovalController.php
+++ src/Domain/Position/Http/Controllers/PositionExternalApprovalController.php
@@ -6,12 +6,16 @@ namespace Domain\Position\Http\Controllers;
 
 use App\Enums\ResponseCodeEnum;
 use App\Http\Controllers\ApiController;
+use Domain\Company\Models\CompanyContact;
 use Domain\Position\Http\Request\PositionExternalApprovalDecideRequest;
 use Domain\Position\Http\Request\PositionExternalApprovalShowRequest;
-use Domain\Position\Http\Resources\PositionExternalResource;
+use Domain\Position\Http\Resources\PositionResource;
+use Domain\Position\Models\PositionApproval;
 use Domain\Position\Repositories\PositionApprovalRepositoryInterface;
 use Domain\Position\UseCases\PositionApprovalDecideUseCase;
+use Domain\User\Models\User;
 use Illuminate\Http\JsonResponse;
+use Support\File\Models\File;
 
 class PositionExternalApprovalController extends ApiController
 {
@@ -26,8 +30,15 @@ class PositionExternalApprovalController extends ApiController
 
         abort_if(!$approval, code: 404);
 
+        // set empty relations, so we don't leak data
+        $approval->position->setRelation('files', modelCollection(File::class));
+        $approval->position->setRelation('hiringManagers', modelCollection(User::class));
+        $approval->position->setRelation('approvers', modelCollection(User::class));
+        $approval->position->setRelation('externalApprovers', modelCollection(CompanyContact::class));
+        $approval->position->setRelation('approvals', modelCollection(PositionApproval::class));
+
         return $this->jsonResponse(ResponseCodeEnum::SUCCESS, [
-            'position' => new PositionExternalResource($approval->position),
+            'position' => new PositionResource($approval->position),
         ]);
     }
 
