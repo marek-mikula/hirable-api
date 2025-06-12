2c2e1b17936ec9dab877a22e979174f6ca4b7e18
Marek Mikula <marek@justitpro.com>
2025-06-12 16:21:08 +0200

    feat: added approval test

---
+++ tests/Process/ApprovalProcessTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Process;
+
+it('tests position approval process', function (): void {
+    // process contains these steps:
+    // todo
+})->todo();
