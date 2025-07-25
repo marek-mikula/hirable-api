a83e61994abea2a1ae334f56d9e8299cbedd8144
Marek Mikula <marek@justitpro.com>
2025-07-25 22:07:52 +0200

    feat: test

---
+++ tests/Common/Enums/TestEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Common\Enums;
+
+enum TestEnum: string
+{
+    case VALUE1 = 'value1';
+    case VALUE2 = 'value2';
+}
--- tests/Unit/App/Casts/EnumOrValueCastTest.php
+++ tests/Unit/App/Casts/EnumOrValueCastTest.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Tests\Unit\App\Casts;
 
+use App\Casts\EnumOrValue;
+use Tests\Common\Enums\TestEnum;
+use Tests\Common\Models\TestModel;
+
+use function PHPUnit\Framework\assertSame;
+
 /**
  * @covers \App\Casts\EnumOrValue::get
  * @covers \App\Casts\EnumOrValue::set
  */
-it('correctly casts value', function (): void {})->todo();
+it('correctly casts value', function (): void {
+    $cast = new EnumOrValue(TestEnum::class);
+    $model = new TestModel();
+
+    // test that value gets cast when setting the value
+    assertSame(TestEnum::VALUE1->value, $cast->set($model, 'value', TestEnum::VALUE1, []));
+    assertSame('randomValue', $cast->set($model, 'value', 'randomValue', []));
+
+    // test that value gets cast when accessing the value
+    assertSame(TestEnum::VALUE1, $cast->get($model, 'value', TestEnum::VALUE1->value, []));
+    assertSame('randomValue', $cast->get($model, 'value', 'randomValue', []));
+})->only();
