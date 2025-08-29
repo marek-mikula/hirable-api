75b3d5ca48d839dce1b46b6cdcdf18b3071132d0
Marek Mikula <marek@justitpro.com>
2025-08-29 11:22:47 +0200

    feat: added classifier test types

--- src/Support/Classifier/Database/Seeders/ClassifierTestTypeDatabaseSeeder.php
+++ src/Support/Classifier/Database/Seeders/ClassifierTestTypeDatabaseSeeder.php
@@ -11,31 +11,12 @@ class ClassifierTestTypeDatabaseSeeder extends AbstractClassifierDatabaseSeeder
     protected function getValues(): array
     {
         return [
-            'logical_reasoning',
-            'verbal_reasoning',
-            'numerical_reasoning',
-            'analytical_thinking',
-            'personality_test',
-            'psychological_test',
-            'work_style_test',
-            'problem_solving',
-            'english_language_test',
-            'foreign_language_test',
-            'technical_knowledge_test',
-            'nontechnical_knowledge_test',
-            'computer_skills_test',
-            'ms_office_test',
-            'programming_test',
-            'sales_skills_test',
-            'real_situation_simulation',
-            'assessment_center',
-            'manual_skills_test',
-            'attention_test',
-            'memory_test',
-            'time_management_test',
-            'values_motivation_test',
-            'stress_resistance_test',
-            'other_test',
+            'language',
+            'expertise',
+            'psychometric',
+            'skills',
+            'case_study',
+            'other',
         ];
     }
 
